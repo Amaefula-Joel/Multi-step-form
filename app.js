be6a58e71b7650656a852c1ef3492a149a3385ba
Joel Amaefula <joelamaefula@gmail.com>
2023-02-07 15:12:38 +0100

    responsiveness and first step in the form is styled

--- app.js
+++ app.js
@@ -2,19 +2,22 @@ var currentTab = 0; // Current tab is set to be the first tab (0)
 showTab(currentTab); // Display the current tab
 
 function showTab(n) {
+  let prevBtn = document.getElementById("prevBtn");
+  let nextBtn = document.getElementById("nextBtn");
+  
   // This function will display the specified tab of the form ...
   var x = document.getElementsByClassName("tab");
   x[n].style.display = "block";
   // ... and fix the Previous/Next buttons:
   if (n == 0) {
-    document.getElementById("prevBtn").style.display = "none";
+    prevBtn.style.display = "none";
   } else {
-    document.getElementById("prevBtn").style.display = "inline";
+    prevBtn.style.display = "";
   }
   if (n == (x.length - 1)) {
-    document.getElementById("nextBtn").innerHTML = "Submit";
+    nextBtn.innerHTML = "Submit";
   } else {
-    document.getElementById("nextBtn").innerHTML = "Next Step";
+    nextBtn.innerHTML = "Next Step";
   }
   // ... and run a function that displays the correct step indicator:
   fixStepIndicator(n)
@@ -54,19 +57,23 @@ function validateForm() {
       valid = false;
     }
   }
-  // If the valid status is true, mark the step as finished and valid:
-  if (valid) {
-    document.getElementsByClassName("step")[currentTab].className += " finish";
-  }
   return valid; // return the valid status
 }
 
 function fixStepIndicator(n) {
   // This function removes the "active" class of all steps...
-  var i, x = document.getElementsByClassName("step");
+  var i, x = document.getElementsByClassName("indicator");
   for (i = 0; i < x.length; i++) {
     x[i].className = x[i].className.replace(" active", "");
   }
   //... and adds the "active" class to the current step:
   x[n].className += " active";
+}
+
+// resets the class of inputs
+function classReset(e){
+  // checks if the input alredy has the class invalid
+  if (e.classList.contains("invalid")) {
+    e.classList.remove("invalid");
+  }
 }
